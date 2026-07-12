ec8b49251a8cae677233ff464b0099c675800655
manith003 <manithkumar8190@gmail.com>
2026-07-12 13:43:06 +0530

    day5 done

---
+++ 5/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello from backend",
+  });
+});
+
+module.exports = app;
---
+++ 5/src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION);
+    console.log("Connected To DB");
+  } catch (error) {
+    console.log("error while connecting to DB", error.message);
+  }
+}
+
+module.exports = connectToDB;
